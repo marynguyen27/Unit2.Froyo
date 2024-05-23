877b1c8df4a63581ce925ceb3a178193f0bbf56b
Mary Nguyen <marynguyen27@gmail.com>
2024-05-22 21:08:00 -0700

    adding function

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-// 1st. Get user input from a prompt
-let flavors = prompt(
-  'Order 6 froyo scoops. The available flavors are vanilla, strawberry, and coffee.'
-);
-console.log('Flavors:', flavors); // looks for a key whatever the prompt is
+function countFlavors() {
+  // 1st. Get user input from a prompt
+  let flavors = prompt(
+    'Order 6 froyo scoops. The available flavors are vanilla, strawberry, and coffee.'
+  );
+  console.log('Flavors:', flavors); // looks for a key whatever the prompt is
 
-// 2nd. Take our prompt input and convert it to something more useful
-//      Prompt input is a STRING
-// Use String method called string.prototype.split(),
-// which takes a string and converts it into an array
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-// promptInput.split(',');
-// this splits string into an array of flavors
+  // 2nd. Take our prompt input and convert it to something more useful
+  //      Prompt input is a STRING
+  // Use String method called string.prototype.split(),
+  // which takes a string and converts it into an array
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+  // promptInput.split(',');
+  // this splits string into an array of flavors
 
-const flavorsArray = flavors.split(',');
-console.log(flavorsArray);
+  const flavorsArray = flavors.split(',');
+  console.log(flavorsArray);
 
-// 3rd. ARRAY to OBJ
-// Loop through array
-//     object[flavor] = assignedvalue
-//     assign a key if it's not there and give it a value
-//     if it is already there, we need to increment the count of how many by 1
+  // 3rd. ARRAY to OBJ
+  // Loop through array
+  //     object[flavor] = assignedvalue
+  //     assign a key if it's not there and give it a value
+  //     if it is already there, we need to increment the count of how many by 1
 
-const objFlavors = {};
-for (let i = 0; i < flavorsArray.length; i++) {
-  if (!Object.hasOwn(objFlavors, flavorsArray[i])) {
-    objFlavors[flavorsArray[i]] = 1;
-  } else {
-    objFlavors[flavorsArray[i]] += 1;
+  const objFlavors = {};
+  for (let i = 0; i < flavorsArray.length; i++) {
+    if (!Object.hasOwn(objFlavors, flavorsArray[i])) {
+      objFlavors[flavorsArray[i]] = 1;
+    } else {
+      objFlavors[flavorsArray[i]] += 1;
+    }
   }
+
+  console.log(objFlavors);
+
+  // 4th. Print the object out
+
+  return objFlavors;
 }
 
-console.log(objFlavors);
-
-// 4th. Print the object out
+const froyoCounts = countFlavors();
+console.log('FroYo Counts:', froyoCounts);
